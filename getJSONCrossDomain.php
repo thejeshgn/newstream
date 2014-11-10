ceffd6e181a183132b2954824fdafd5368499025
thejeshgn <i@thejeshgn.com>
2014-11-10 16:46:10 +0530

    changed to curl

--- getJSONCrossDomain.php
+++ getJSONCrossDomain.php
@@ -1,6 +1,14 @@
 <?php
 
 header('Content-type: application/json');
-echo file_get_contents("https://feeds.pinboard.in/json/u:thej/t:chillfactor2014");
+$url = "https://feeds.pinboard.in/json/u:thej/t:chillfactor2014";
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+$output = curl_exec($ch);
+curl_close($ch); 
+            
+echo $output;
 
 ?>
